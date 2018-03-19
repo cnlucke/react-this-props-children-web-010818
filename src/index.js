11876e06cb2c358b19da7935b4e59a1291364a53
cnlucke <cristy.lucke@gmail.com>
2018-03-19 12:53:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Code Goes Here
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Movie from './Movie'
+import MovieBrowser from './MovieBrowser'
+
+ReactDOM.render(
+  <MovieBrowser>
+    <Movie title="Mad Max: Fury Road" />
+    <Movie title="Harry Potter and The Goblet Of Fire" />
+  </MovieBrowser>,
+  document.getElementById('root'));
